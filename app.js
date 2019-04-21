36e301bc74a82efd73d5272090c14eadd6b93e0e
dizzyplay <vovoboss@gmail.com>
2019-04-22 07:44:07 +0900

    fix-

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.get([
-  '/',
-  '/index.html',
-  function(req, res) {
-    res.send('Hello');
-  },
-]);
+app.get(['/', '/index.html'], function(req, res) {
+  res.send('Hello');
+});
 
-app.listen(80);
+app.listen(3006);
